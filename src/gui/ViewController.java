12228c3ea0d09719734b917b69e96ef1bb541310
Marcos Guilherme <marcosfs.guilherme@gmail.com>
2019-10-24 20:17:15 -0300

    Updates: ComboBox

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,16 +1,25 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import gui.util.Alerts;
 import gui.util.Constraints;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.util.Callback;
+import model.entities.Person;
 
 public class ViewController implements Initializable {
 	@FXML
@@ -21,6 +30,7 @@ public class ViewController implements Initializable {
 	private Label labelResult;
 	@FXML
 	private Button btSum;
+
 	@FXML
 	public void onBtSumAction() {
 		try {
@@ -28,19 +38,36 @@ public class ViewController implements Initializable {
 			double numberTwo = Double.parseDouble(txtNumberTwo.getText());
 			double sum = numberOne + numberTwo;
 			labelResult.setText(String.format("%.2f", sum));
-		}
-		catch (NumberFormatException e) {
+		} catch (NumberFormatException e) {
 			Alerts.showAlert("Error", "Parse error", e.getMessage(), AlertType.ERROR);
 		}
 	}
-	
-	
+
 	@FXML
 	private Button btTest;
-	
+
 	@FXML
 	public void onBtTestAction() {
-		Alerts.showAlert("Alert title", "Alert header" , "Alert content", AlertType.INFORMATION);
+		Alerts.showAlert("Alert title", "Alert header", "Alert content", AlertType.INFORMATION);
+	}
+
+	@FXML
+	private Button btAll;
+	
+	@FXML
+	private ComboBox<Person> comboBoxPerson;
+	private ObservableList<Person> obsList;
+	
+	@FXML
+	public void onBtAllAction() {
+		for(Person p : comboBoxPerson.getItems()) {
+			System.out.println(p);
+		}
+	}
+	@FXML
+	public void onComboBoxPersonAction() {
+		Person person = comboBoxPerson.getSelectionModel().getSelectedItem();
+		System.out.println(person);
 	}
 
 	@Override
@@ -49,5 +76,23 @@ public class ViewController implements Initializable {
 		Constraints.setTextFieldDouble(txtNumberTwo);
 		Constraints.setTextFieldMaxLength(txtNumberOne, 12);
 		Constraints.setTextFieldMaxLength(txtNumberTwo, 12);
+
+		List<Person> list = new ArrayList<>();
+		list.add(new Person(1, "Maria", "maria@gmail.com"));
+		list.add(new Person(1, "Alex", "alex@gmail.com"));
+		list.add(new Person(1, "Bob", "bob@gmail.com"));
+
+		obsList = FXCollections.observableArrayList(list);
+		comboBoxPerson.setItems(obsList);
+
+		Callback<ListView<Person>, ListCell<Person>> factory = lv -> new ListCell<Person>() {
+			@Override
+			protected void updateItem(Person item, boolean empty) {
+				super.updateItem(item, empty);
+				setText(empty ? "" : item.getName());
+			}
+		};
+		comboBoxPerson.setCellFactory(factory);
+		comboBoxPerson.setButtonCell(factory.call(null));
 	}
 }
